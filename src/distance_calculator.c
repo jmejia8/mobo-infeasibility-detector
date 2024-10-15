3fa26f9dd601e49310a8a52e048c819c46897a6d
Jesus Mejia <jesusmejded@gmail.com>
2024-10-15 08:36:22 -0600

    improve distance calculation

--- src/distance_calculator.c
+++ src/distance_calculator.c
@@ -24,18 +24,19 @@ float* compute_distance_matrix(float *X, int nrow, int ncol) {
     // Allocate memory for the distance matrix (nrow x nrow)
     float *dist_matrix = (float*) malloc(sizeof(float) * nrow * nrow);
     if (dist_matrix == NULL) {
-        printf("Memory allocation failed\n");
+        printf("Error: Memory allocation failed (distance matrix)\n");
+        //printf("%d, %d^2 = %d\n", sizeof(float), nrow, nrow*nrow);
+        printf("\tMemory required: %.4f GB\n", (double)(sizeof(float) * nrow * nrow) * (double)  9.3132257461548E-10);
         exit(1);
     }
 
     // Compute the distance between each pair of rows
     for (int i = 0; i < nrow; ++i) {
-        for (int j = 0; j < nrow; ++j) {
-            if (i == j) {
-                dist_matrix[i * nrow + j] = 0.0;  // Distance to self is 0
-            } else {
-                dist_matrix[i * nrow + j] = manhattan_distance(X, ncol, i, j);
-            }
+        dist_matrix[i * nrow + i] = 0.0;  // Distance to self is 0
+        for (int j = i+1; j < nrow; ++j) {
+            float d = manhattan_distance(X, ncol, i, j);
+            dist_matrix[i * nrow + j] = d;
+            dist_matrix[j * nrow + i] = d;
         }
     }
 
